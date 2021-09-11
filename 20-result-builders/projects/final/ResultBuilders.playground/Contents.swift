19a07ba6e044307ac9497897f8942048a61f9c44
Steven Van Impe <steven.vanimpe@icloud.com>
2021-09-11 13:23:43 +0200

    Tech edit

--- 20-result-builders/projects/final/ResultBuilders.playground/Contents.swift
+++ 20-result-builders/projects/final/ResultBuilders.playground/Contents.swift
@@ -1,6 +1,36 @@
-import UIKit
+/// Copyright (c) 2021 Razeware LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
 
-var greeting = "Hello, playground"
+import UIKit
 
 func greet(name: String) -> NSAttributedString {
   let attributes = [NSAttributedString.Key.foregroundColor : UIColor.red]
@@ -29,7 +59,7 @@ enum AttributedStringBuilder {
   }
 
   static func buildOptional(_ component: NSAttributedString?) -> NSAttributedString {
-    return component ?? NSAttributedString()
+    component ?? NSAttributedString()
   }
 
   static func buildEither(first component: NSAttributedString) -> NSAttributedString {
@@ -58,7 +88,7 @@ enum AttributedStringBuilder {
   }
 
   static func buildExpression(_ expression: NSAttributedString) -> NSAttributedString {
-    return expression
+    expression
   }
 }
 
@@ -93,12 +123,12 @@ extension NSMutableAttributedString {
 func greetBuilder(name: String, title: String) -> NSAttributedString {
   Text("Hello ")
   Text(name)
-    .color(UIColor.red)
+    .color(.red)
   if !title.isEmpty {
     Text(", ")
     Text(title)
-      .font(UIFont.systemFont(ofSize: 20))
-      .color(UIColor.blue)
+      .font(.systemFont(ofSize: 20))
+      .color(.blue)
   } else {
     Text(", No title")
   }
@@ -110,14 +140,14 @@ greetBuilder(name: "Daenerys", title: "Mother of dragons")
 func greetBuilder(name: String, titles: [String]) -> NSAttributedString {
   Text("Hello ")
   Text(name)
-    .color(UIColor.red)
+    .color(.red)
   if !titles.isEmpty {
     for title in titles {
       SpecialCharacters.comma
       SpecialCharacters.lineBreak
       Text(title)
-        .font(UIFont.systemFont(ofSize: 20))
-        .color(UIColor.blue)
+        .font(.systemFont(ofSize: 20))
+        .color(.blue)
     }
   } else {
     Text(", No title")
