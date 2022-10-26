a445762bb024cda93a868816d99bbb37bc8fef1d
Walter Tyree <walter@tyreeapps.com>
2022-10-26 16:18:06 -0400

    update copyright match chapter text

--- 14-classes/projects/final/14-classes.playground/Contents.swift
+++ 14-classes/projects/final/14-classes.playground/Contents.swift
@@ -1,15 +1,15 @@
-/// Copyright (c) 2021 Razeware LLC
-/// 
+/// Copyright (c) 2022 Kodeco LLC
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -81,7 +81,7 @@ area1.range = 4
 print(area1.range) // 4.0
 print(area2.range) // 2.5
 
-var homeOwner = john
+var homeOwner = john  // "Johnny Appleseed"
 john.firstName = "John" // John wants to use his short name!
 john.firstName // "John"
 homeOwner.firstName // "John"
@@ -157,7 +157,7 @@ class Student {
 }
 
 let jane = Student(firstName: "Jane", lastName: "Appleseed")
-let history = Grade(letter: "B", points: 9.0, credits: 3.0)
+var history = Grade(letter: "B", points: 9.0, credits: 3.0)
 var math = Grade(letter: "A", points: 16.0, credits: 4.0)
 
 jane.recordGrade(history)
