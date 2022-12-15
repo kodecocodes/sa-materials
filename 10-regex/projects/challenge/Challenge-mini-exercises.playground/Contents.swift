2026632a1db2806a41be68e6b166ffce73a51d4a
Walter Tyree <walter@tyreeapps.com>
2022-12-14 20:32:11 -0500

    added copyrights

--- 10-regex/projects/challenge/Challenge-mini-exercises.playground/Contents.swift
+++ 10-regex/projects/challenge/Challenge-mini-exercises.playground/Contents.swift
@@ -30,88 +30,10 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import RegexBuilder
+/*: ## Mini-Exercises
 
-let capsRegex = Regex {
-  Anchor.wordBoundary
-  ZeroOrMore {
-    CharacterClass.digit.union("a"..."z")
-  }
-  Capture {
-    OneOrMore {
-      "A"..."Z"
-    }
-  }
-  ZeroOrMore {
-    CharacterClass.digit.union("a"..."z")
-  }
-  Capture {
-    ZeroOrMore {
-      "A"..."Z"
-    }
-  }
-  ZeroOrMore {
-    OneOrMore {
-      CharacterClass.digit.union("a"..."z")
-    }
-    Capture {
-      ZeroOrMore {
-        "A"..."Z"
-      }
-    }
-  }
-  Anchor.wordBoundary
-}
+ Now that you've learned how to construct regular expressions with some different capabilities, how would you adapt `/[a-z]+[0-9]+/` from earlier to match all of the example texts `abcd12345`, `swiftapprentice2023`, `XYZ567`, `Pennsylvania65000`?
 
-let testingString1 = "abcdef ABCDEF 12345 abc123 ABC 123 123ABC 123abc abcABC"
+ */
 
-print("Test 1:")
-
-for match in testingString1.matches(of: capsRegex) {
-  print(match.output)
-}
-
-print("----------------")
-print("Test 2:")
-
-let testingString2 = "abcdeABCDE1234 1234ABCDEabcde abcde1234"
-
-for match in testingString2.matches(of: capsRegex) {
-  print(match.output)
-}
-
-print("----------------")
-print("Test 3:")
-
-let testingString3 = "a1b2ABCDEc3d4"
-
-for match in testingString3.matches(of: capsRegex) {
-  print(match.output)
-}
-
-print("----------------")
-print("Test 4:")
-
-let testingString4 = "a1b2ABCDEc3d4FGHe5f6g7"
-
-for match in testingString4.matches(of: capsRegex) {
-  print(match.output)
-}
-
-print("----------------")
-print("Test 5:")
-
-let testingString5 = "a1b2ABCDEc3d4FGHe5f6g7IJK"
-
-for match in testingString5.matches(of: capsRegex) {
-  print(match.output)
-}
-
-print("----------------")
-print("Test 6:")
-
-let testingString6 = "a1b2ABCDEc3d4FGHe5f6g7IJKh8i9LMNj0OPQ"
-
-for match in testingString6.matches(of: capsRegex) {
-  print(match.output)
-}
+//The regex needs to account for upper-case letters, so changing it to /[A-Z]+[0-9]+/ or even just /\w+/ would match all of the examples.
--- 10-regex/projects/final/10-regex.playground/Contents.swift
+++ 10-regex/projects/final/10-regex.playground/Contents.swift
@@ -1,3 +1,47 @@
+/// Copyright (c) 2023 Kodeco LLC
+///
+/// Permission is hereby granted, free of charge, to any person obtaining a copy
+/// of this software and associated documentation files (the "Software"), to deal
+/// in the Software without restriction, including without limitation the rights
+/// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+/// copies of the Software, and to permit persons to whom the Software is
+/// furnished to do so, subject to the following conditions:
+///
+/// The above copyright notice and this permission notice shall be included in
+/// all copies or substantial portions of the Software.
+///
+/// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
+/// distribute, sublicense, create a derivative work, and/or sell copies of the
+/// Software in any work that is designed, intended, or marketed for pedagogical or
+/// instructional purposes related to programming, coding, application development,
+/// or information technology.  Permission for such use, copying, modification,
+/// merger, publication, distribution, sublicensing, creation of derivative works,
+/// or sale is expressly withheld.
+///
+/// This project and source code may use libraries or frameworks that are
+/// released under various Open-Source licenses. Use of those libraries and
+/// frameworks are governed by their own individual licenses.
+///
+/// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+/// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+/// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+/// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+/// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+/// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+/// THE SOFTWARE.
+
+//:## Regular Expressions
+let searchString = "john"
+let searchExpression = /john/
+
+let stringToSearch = "Johnny Appleseed wants to change his name to John."
+
+print(stringToSearch.contains(searchExpression))
+print(stringToSearch.contains(searchString))
+
+let flexibleExpression = /[Jj]ohn/
+print(stringToSearch.contains(flexibleExpression))
+
 let testingString1 = "abcdef ABCDEF 12345 abc123 ABC 123 123ABC 123abc abcABC"
 let lettersAndNumbers = /[a-z]+[0-9]+/
 
@@ -16,7 +60,7 @@ for match in testingString1.matches(of: possibleLettersAndPossibleNumbers) {
 print("------------------")
 
 let emptyString = ""
-let count =  emptyString.matches(of: possibleLettersAndPossibleNumbers).count
+let matchCount =  emptyString.matches(of: possibleLettersAndPossibleNumbers).count
 
 let fixedPossibleLettersAndPossibleNumbers = /[a-z]+[0-9]*|[a-z]*[0-9]+/
 
@@ -85,18 +129,47 @@ let regexWithCapture = Regex {
   OneOrMore {
     "a"..."z"
   }
-  Capture {
+  TryCapture {
     OneOrMore {
       CharacterClass.digit
     }
-  }
+  } transform: {foundDigit in
+      Int(foundDigit)
+    }
+
   OneOrMore {
     "a"..."z"
   }
 }
 
-let testingString2 = "welc0me to chap7er 10 in sw1ft appren71ce. " +
+let testingString2 = "Welc0me to chap7er 10 in sw1ft appren71ce. " +
   "Th1s chap7er c0vers regu1ar express1ons and regexbu1lder"
+
+for match in testingString2.matches(of: regexWithCapture) {
+  print(match.output)
+}
+
+for match in testingString2.matches(of: regexWithCapture) {
+  let (wordMatch, extractedDigit) = match.output
+  print("Full Match: \(wordMatch) | Captured value: \(extractedDigit)")
+}
+
+let regexWithStrongType = Regex {
+  OneOrMore {
+    "a"..."z"
+  }
+  TryCapture {
+    OneOrMore {
+      CharacterClass.digit
+    }
+  } transform: {foundDigit in
+      Int(foundDigit)
+    }
+
+  OneOrMore {
+    "a"..."z"
+  }
+}
 
 for match in testingString2.matches(of: regexWithCapture) {
   print(match.output)
@@ -122,3 +195,6 @@ let repeatedCaptures = Regex {
 for match in repetition.matches(of: repeatedCaptures) {
   print(match.output)
 }
+
+
+
