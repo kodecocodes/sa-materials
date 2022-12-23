b241fbc7f93f6668ae9304f34f04995c25f3490d
Ray Fix <rayfix@gmail.com>
2022-12-22 19:21:47 -0800

    FPE Chapter 13  chapter 12 -> 13

--- 13-methods/projects/final/13-methods.playground/Contents.swift
+++ 13-methods/projects/final/13-methods.playground/Contents.swift
@@ -29,7 +29,7 @@
 /// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
-//: ## Chapter 12: Methods
+//: ## Chapter 13: Methods
 var numbers = [1, 2, 3]
 numbers.removeLast()
 numbers
